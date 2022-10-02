ece20b3787f7f1a158a8bbd830480828bc951f84
aslanliIbrahim <ibrahimra@code.edu.az>
2022-10-02 19:15:49 +0400

    Section two and one is ready for use

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,35 +3,103 @@ const arrowNav = document.querySelectorAll(".arrow-nav");
 const subMenu = document.querySelectorAll(".sub-menu");
 
 for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", function () {
+    // if (arrowNav.classList.contains("arrow-nav-rotate")) {
+    //     arrowNav.classList.remove("arrow-nav-rotate");
+    // } else {
+    //     arrowNav.classList.add("arrow-nav-rotate");
+    // }
 
-    button[i].addEventListener('click', function(e){
+    for (let j = 0; j < arrowNav.length; j++) {
+
+      arrowNav[i].addEventListener("click", function () {
         
-      for (let j = 0; j < arrowNav.length; j++) {
-
-        if (arrowNav[i].classList.contains("arrow-nav-rotate")) {
+        if (this.classList.contains("arrow-nav-rotate")) {
             
-            arrowNav[j].classList.remove('arrow-nav-rotate');
-            for (let h = 0; h < subMenu.length; h++) {
-                subMenu[h].classList.add('sub-menu-active');
-                subMenu[h].classList.add('sub-menu');
+          this.classList.remove("arrow-nav-rotate");
 
-            }
+        } else {
 
-        }else{
-            
-            arrowNav[j].classList.add('arrow-nav-rotate');
-            for (let h = 0; h < subMenu.length; h++) {
-                subMenu[h].classList.add('sub-menu-active');
-                subMenu[h].classList.remove('sub-menu');
-
-            }
-
+          this.classList.add("arrow-nav-rotate");
+          
         }
-      }
-    })
+      });
+    }
+  });
 }
 
+// for (let i = 0; i < button.length; i++) {
 
+//     button[i].addEventListener('click', function() {
+
+//         for (let j = 0; j < arrowNav.length; j++) {
+
+//             if (arrowNav[j].classList.contains("arrow-nav-rotate")) {
+
+//                 arrowNav[i].classList.remove("arrow-nav-rotate");
+//             } else {
+//                 arrowNav[i].classList.add("arrow-nav-rotate");
+//             }
+//             // arrowNav[j].addEventListener('click', function(){
+//             //     if ( this.classList.contains("arrow-nav-rotate")) {
+//             //         this.classList.remove("arrow-nav-rotate");
+//             //        } else {
+//             //         this.classList.add("arrow-nav-rotate");
+//             //     }
+//             // })
+
+//         }
+
+//     })
+
+// }
+
+// for (let i = 0; i < arrowNav.length; i++) {
+
+//     arrowNav[i].addEventListener("click", function() {
+//         console.log(this);
+
+//         for (let j = 0; j < button.length; j++) {
+
+//             button[i].addEventListener("click", function() {
+
+//                 console.log(this, "from button");
+
+//             })
+
+//         }
+//     })
+
+// }
+
+// for (let i = 0; i < button.length; i++) {
+
+//     button[i].addEventListener('click', function(e){
+
+//       for (let j = 0; j < arrowNav.length; j++) {
+
+//         if (arrowNav[i].classList.contains("arrow-nav-rotate")) {
+
+//             arrowNav[j].classList.remove('arrow-nav-rotate');
+//             for (let h = 0; h < subMenu.length; h++) {
+//                 subMenu[h].classList.add('sub-menu-active');
+//                 subMenu[h].classList.add('sub-menu');
+
+//             }
+
+//         }else{
+
+//             arrowNav[j].classList.add('arrow-nav-rotate');
+//             for (let h = 0; h < subMenu.length; h++) {
+//                 subMenu[h].classList.add('sub-menu-active');
+//                 subMenu[h].classList.remove('sub-menu');
+
+//             }
+
+//         }
+//       }
+//     })
+// }
 
 // button.forEach((el)=>{
 //     el.addEventListener("click",(e)=>{
@@ -39,7 +107,3 @@ for (let i = 0; i < button.length; i++) {
 
 //     })
 // })
-
-
-
-
