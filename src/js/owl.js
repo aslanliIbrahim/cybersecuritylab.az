c77fa44204b7b4e15986920d90a9d07afad29e6e
aslanliIbrahim <ibrahimra@code.edu.az>
2022-10-07 14:24:23 +0400

    Slider area is ready for use

---
+++ src/js/owl.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        center:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false
+    });
+  });
