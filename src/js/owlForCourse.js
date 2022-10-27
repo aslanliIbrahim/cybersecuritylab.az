34bbcb371b64680d992215fcbde20fd066dc2f05
aslanliIbrahim <ibrahimra@code.edu.az>
2022-10-27 10:02:25 +0400

    Some bugs are fixed again

--- src/js/owlForCourse.js
+++ src/js/owlForCourse.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
-        // nav:true,
         items:1,
         loop:true,
         center:true,
         // autoplay:true,
         // autoplayTimeout:3000,
         dots:false,
-       
+        // nav:true,
     });
   });
