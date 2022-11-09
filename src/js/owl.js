110764b102bd1b01a5301bcf278cd0c7246ba5d4
aslanliIbrahim <ibrahimra@code.edu.az>
2022-11-09 14:14:41 +0400

    Slider is fixed again

--- src/js/owl.js
+++ src/js/owl.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
         items:1,
         loop:true,
         center:true,
-        // autoplay:true,
-        // autoplayTimeout:3000,
+        autoplay:true,
+        autoplayTimeout:3000,
         dots:false,
        
     });
--- src/js/owlForCourse.js
+++ src/js/owlForCourse.js
@@ -1,11 +1,32 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+
+
+  $(document).ready(function(){
+    $(".one-slide").owlCarousel({
+        nav:false,
         items:1,
         loop:true,
         center:true,
-        // autoplay:true,
-        // autoplayTimeout:3000,
+        autoplay:true,
+        autoplayTimeout:3000,
         dots:false,
-        // nav:true,
+       
     });
   });
+
+  const nextIcon = '<img class="for-nav-img" src="../src/img/course/right-arrow.png" alt="next">';
+  const prevIcon ='<img class="for-nav-img" src="../src/img/course/arrow.png" alt="prev">';
+
+$(document).ready(function () {
+ 
+  $(".two-slide").owlCarousel({
+    nav: false,
+    navText: [prevIcon, nextIcon],
+    items: 1,
+    // loop:true,
+    center: true,
+    // autoplay:true,
+    // autoplayTimeout:3000,
+    dots: false,
+  });
+});
+
